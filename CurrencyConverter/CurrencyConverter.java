56cbd5e86c8aac54339b4ec17f2cf6e751972911
khoirunnisapk <48423384+khoirunnisapk@users.noreply.github.com>
2020-02-22 18:55:31 +0700

    Add files via upload

---
+++ CurrencyConverter/CurrencyConverter.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// ***********************************************************************
+// CurrencyConverter.java
+//
+// Computes the dollar value of the cost of an item in another currency.
+// ***********************************************************************
+import java.awt.*;
+import javax.swing.*;
+public class CurrencyConverter
+{
+public static void main (String[] args)
+{
+JFrame frame = new JFrame ("Currency Converter");
+frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+RatePanel ratePanel = new RatePanel ();
+frame.getContentPane().add(ratePanel);
+frame.pack();
+frame.setSize(400, 200);
+frame.setVisible(true);
+}
+}
